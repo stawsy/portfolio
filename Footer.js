9e672ef8f00198f43d7f378d6804cdfd6a8e24cf
stawsy <45562525+stawsy@users.noreply.github.com>
2025-10-29 23:33:51 +1100

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,21 @@
+//import { Link } from 'react-router-dom';
+import React from 'react'
+
+const Footer = () => {
+  return (
+      
+<>
+      
+<div class="footer p-2 position-sticky bottom-0 end-0">
+
+    <div class="container text-center">
+        <h4>Footer</h4>
+    </div>
+
+</div>
+      
+</>   
+      
+  );
+};
+export default Footer;
---
+++ Navbar.js
@@ -0,0 +1,54 @@
+//import { Link } from 'react-router-dom';
+import React from 'react'
+
+const Navbar = () => {
+  return (
+      
+<>
+  
+<div class="header p-5 pb-0 position-sticky top-0 start-0">
+      
+      <div class="container text-center">
+        <h1>&lt;Steven Bustos /&gt;</h1>
+      </div>
+      
+      <div class="block text-center position-relative clouds">
+      
+        <svg width="100%" height="120" xmlns="http://www.w3.org/2000/svg" viewBox="900 0 120 120">
+
+        <g>
+            <title>Layer 1</title>
+            <ellipse ry="48.5" rx="102.5" id="svg_6" cy="61.5" cx="778.45647" fill="#fb452b"/>
+            <ellipse ry="48.5" rx="102.5" id="svg_14" cy="61.5" cx="150.38234" fill="#fb452b"/>
+            <ellipse ry="48.5" rx="102.5" id="svg_10" cy="61.5" cx="946.45876" fill="#fb452b"/>
+            <ellipse ry="48.5" rx="102.5" id="svg_15" cy="61.5" cx="336.44954" fill="#fb452b"/>
+            <ellipse ry="48.5" rx="102.5" id="svg_28" cy="61.5" cx="1132.52596" fill="#fb452b"/>
+            <ellipse ry="48.5" rx="102.5" id="svg_8" cy="61.5" cx="1520.53111" fill="#fb452b"/>
+            <ellipse ry="48.5" rx="102.5" id="svg_16" cy="61.5" cx="574.85658" fill="#fb452b"/>
+            <ellipse ry="48.5" rx="102.5" id="svg_29" cy="61.5" cx="1303.07906" fill="#fb452b"/>
+            <ellipse ry="48.5" rx="102.5" id="svg_3" cy="61.5" cx="1691.08421" fill="#fb452b"/>
+            <ellipse stroke="null" ry="48.5" rx="55" id="svg_17" cy="61.5" cx="55" fill="#fb452b"/>
+            <ellipse stroke="null" ry="48.5" rx="55" id="svg_7" cy="61.5" cx="851.07641" fill="#fb452b"/>
+            <ellipse stroke="null" ry="48.5" rx="55" id="svg_18" cy="48.5" cx="253.00229" fill="#fb452b"/>
+            <ellipse stroke="null" ry="48.5" rx="55" id="svg_11" cy="51.5" cx="1049.0787" fill="#fb452b"/>
+            <ellipse stroke="null" ry="48.5" rx="55" id="svg_19" cy="61.5" cx="459.06949" fill="#fb452b"/>
+            <ellipse stroke="null" ry="48.5" rx="55" id="svg_30" cy="61.5" cx="1235.1459" fill="#fb452b"/>
+            <ellipse stroke="null" ry="48.5" rx="55" id="svg_9" cy="51.5" cx="1613.15106" fill="#fb452b"/>
+            <ellipse stroke="null" ry="48.5" rx="55" id="svg_20" cy="71.5" cx="677.47653" fill="#fb452b"/>
+            <ellipse stroke="null" ry="48.5" rx="55" id="svg_31" cy="61.5" cx="1405.699" fill="#fb452b"/>
+            <ellipse stroke="null" ry="48.5" rx="55" id="svg_2" cy="71.5" cx="1793.70416" fill="#fb452b"/>
+            <path id="svg_21" d="m36.88005,84" opacity="NaN" stroke="null" fill="#fb452b"/>
+            <path id="svg_12" d="m832.95647,84" opacity="NaN" stroke="null" fill="#fb452b"/>
+        </g>
+      
+        </svg>
+      
+      </div>
+
+</div>
+      
+</>   
+      
+  );
+};
+export default Navbar;
